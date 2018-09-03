9a1b198feb4edd68504934b68823504c36c3e126
Ли Лилия <melifarofrei@gmail.com>
2018-09-03 17:58:34 +0300

    First commit

---
+++ VideoChat/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoChat.Models;
+
+namespace VideoChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AccountDbContext db = new AccountDbContext("Accounts");
+        
+        public ActionResult Index()
+        {
+            var s = db.accounts.ToList();
+            db.accounts.Add(new Account("1234", "firstEverUser", "looser@mail.ru", 1));
+            db.SaveChanges();
+            return View(db.accounts);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VideoChat/Models/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoChat.Models
+{
+    [Table("Account")]
+    public class Account
+    {
+        [Column("Password")]
+        public string password { get; protected set; }
+
+        [Column("Name")]
+        public string userName { get; protected set; }
+
+        [Column("Email")]
+        public string email { get; protected set; }
+
+        [Key]
+        public int ID { get; protected set; }
+
+        public Account(string password = "x", string username = "y", string email = "i", int id = 0)
+        {
+            this.password = password;
+            this.userName = userName;
+            this.email = email;
+            this.ID = id;
+        }
+
+        public Account()
+        { }
+    }
+}
